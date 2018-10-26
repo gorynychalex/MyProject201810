7cb75798d7033bfc25fb1afb294016790f32a25b
Alexey Popovich <gorynychalex@gmail.com>
2018-10-26 20:40:53 +1000

    new commint

--- src/ru/popovich/java/Main01.java
+++ src/ru/popovich/java/Main01.java
@@ -3,5 +3,11 @@ package ru.popovich.java;
 public class Main01 {
     public static void main(String[] args) {
         System.out.println("Hello from new project");
+
+
+    }
+
+    public static void method(){
+        
     }
 }
